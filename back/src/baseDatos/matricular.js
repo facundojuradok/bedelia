ea612c085f109bfb9e3926457deecafd56fb0469
Luka <lukaliuzzidev@gmail.com>
2023-10-30 16:45:08 -0300

    matricular

--- back/src/baseDatos/matricular.js
+++ back/src/baseDatos/matricular.js
@@ -1,20 +1,19 @@
-const conexion = require('./conexionBD');
+const conexion = require("./conexionBD")
 
-const matricularEstudiante = async(estudiante, carrera, materias) =>{
-    //Agregamos la carrera
-    const consulta = "INSERT INTO estudiantecarrera SET ?"
-    await conexion.query(consulta, {estudiante, carrera});
+const matricularEstudiante = async (estudiante, carrera, materias) => {
+  //Agregamos la carrera
+  const consulta = "INSERT INTO estudiantecarrera SET ?"
+  await conexion.query(consulta, { estudiante, carrera, fechaAlta: new Date() })
 
-    //Luego agregamos las materias
-    for(const materia of materias){
-        const consulta2 = "INSERT INTO estudiantemateria SET ?"
-        await conexion.query(consulta2, { estudiante, materia });
-    }
+  //Luego agregamos las materias
+  for (const materia of materias) {
+    const consulta2 = "INSERT INTO estudiantemateria SET ?"
+    await conexion.query(consulta2, { estudiante, materia, fecha: new Date() })
+  }
 
-    return {mensaje: "Estudiante matriculado correctamente"}
-
+  return { mensaje: "Estudiante matriculado correctamente" }
 }
 
 module.exports = {
-    matricularEstudiante,
+  matricularEstudiante,
 }
